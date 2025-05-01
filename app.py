7583c67202faa6364cbe42bf23bf9e6d153fa197
Solutionsnextwave <gogulan@solutionsnext.in>
2025-05-02 03:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-
 from flask import Flask, render_template, request, redirect, session, url_for
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
 import os
 
